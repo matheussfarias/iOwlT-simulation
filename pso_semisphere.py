6ef6065b38b9604511fc24efad17c009149129fc
matheussfarias <msf4@cin.ufpe.br>
2020-05-22 01:06:30 -0300

    pso with semisphere

--- pso_semisphere.py
+++ pso_semisphere.py
@@ -9,6 +9,7 @@ Created on Thu May 21 20:33:41 2020
 import numpy as np
 from multilateration_algorithms import *
 import matplotlib.pyplot as plt
+import itertools
 
 population = []  
 
@@ -22,11 +23,10 @@ receptorsPositions[1] = np.array([-np.sqrt(2)/2, np.sqrt(2)/2, 0])
 receptorsPositions[2] = np.array([-np.sqrt(2)/2, -np.sqrt(2)/2, 0])
 receptorsPositions[3] = np.array([-np.sqrt(2)/2, np.sqrt(2)/2, 0])
 
-source = np.array([-5, -6, -3])
 
 
 maxit = 100
-quantity = 50
+quantity = 3
 w = 1
 c1 = 2
 c2 = 2
@@ -35,8 +35,9 @@ wdamp = 0.99
 population = []
 globalbest = [float("inf"), 0]
 bestcost= []
-  
+conta=0
 for i in range (0, quantity):
+    print(i)
     r = np.random.rand(1)
     while(r==0 or r>1):
         r = np.random.rand(1)
@@ -46,15 +47,29 @@ for i in range (0, quantity):
     mic = sph2car(r, azimuthe, elevation).reshape((1,3))
 
     receptorsPositions[3] = mic
-    rand = (2*np.random.rand(samples) - 1) # Random vector between 1 and -1
-    delays, _ = delayEstimator (rand, sampleRate, source, receptorsPositions, typeComb = "")
-    result = MLE_HLS(receptorsPositions, delays, sampleRate)
-    distance = float(dist(result, source))
-    population.append([mic, distance, np.zeros((1,3)), mic, distance])
+    distance=0
+    R = np.linspace(1,15,15)
+    phi = np.linspace(0, 2*np.pi,24, endpoint=False)
+    theta = np.linspace(0, np.pi/2, 12, endpoint=True)
     
+    semi_sphere = itertools.product(R, phi, theta)
+
+    for (R, phi, theta) in semi_sphere:
+
+        source = sph2car(R, phi, theta) + np.random.randn(3)*0.1
+        source = np.round(source, decimals=2)
+        
+        rand = (2*np.random.rand(samples) - 1) # Random vector between 1 and -1
+        delays, _ = delayEstimator (rand, sampleRate, source, receptorsPositions, typeComb = "")
+        result = MLE_HLS(receptorsPositions, delays, sampleRate)
+        distance += float(dist(result, source))
+          
+    population.append([mic, distance, np.zeros((1,3)), mic, distance])
+        
     if population[i][4] < globalbest[0]:
         globalbest[0] = population[i][4]
         globalbest[1] = population[i][3]
+      
       
 for it in range(0,maxit):
     for i in range (1, quantity):
@@ -67,9 +82,17 @@ for it in range(0,maxit):
             population[i][1] = float("inf")
         else:
             receptorsPositions[3] = population[i][0]
-            delays, _ = delayEstimator (rand, sampleRate, source, receptorsPositions, typeComb = "")
-            result = MLE_HLS(receptorsPositions, delays, sampleRate)
-            distance = float(dist(result, source))
+            R = np.linspace(1,15,15)
+            phi = np.linspace(0, 2*np.pi,24, endpoint=False)
+            theta = np.linspace(0, np.pi/2, 12, endpoint=True)
+    
+            semi_sphere = itertools.product(R, phi, theta)
+            distance=0
+            for (R, phi, theta) in semi_sphere:
+                delays, _ = delayEstimator (rand, sampleRate, source, receptorsPositions, typeComb = "")
+                result = MLE_HLS(receptorsPositions, delays, sampleRate)
+                distance += float(dist(result, source))
+                
             population[i][1] = distance
             
         if( (population[i][0]==receptorsPositions[0]).all() or (population[i][0]==receptorsPositions[1]).all() or (population[i][0]==receptorsPositions[2]).all() or (population[i][0]==source).all()):
